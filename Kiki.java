694af0b5438c4dd55e338950fa363e89aa8b498c
root <root@debian-s-1vcpu-1gb-nyc1-01.localdomain>
2020-09-26 21:40:18 +0000

    I am taking CS407 because... commit

--- Kiki.java
+++ Kiki.java
@@ -1,5 +1,5 @@
 public class Kiki{
 	public static void main(String[]args){
-		System.out.println("My favorite programming language is JAVA.");
+		System.out.println("I am taking CS407 because I have requirements to meet to graduate.");
 	}
 }
